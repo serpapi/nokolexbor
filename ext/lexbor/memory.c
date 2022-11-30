76c7c366f1989a3ef4fdec83655b75adce3e9001
zyc9012 <zyc9012@163.com>
2022-11-30 10:25:42 +0800

    Fix memory bloat issue

---
+++ ext/lexbor/memory.c
@@ -0,0 +1,39 @@
+/*
+ * This file overrides the default `lexbor_malloc` and other
+ * related functions to reduce memory bloat and allow Ruby to
+ * GC more often.
+ * By calling `ruby_xmalloc` instead of `malloc`, Ruby can
+ * count the memory usage of the C extension and run GC
+ * whenever `malloc_increase_byte` exceeds the limit. Similar
+ * to Nokogiri's solution by calling `xmlMemSetup`.
+ * The downside will be the downgrade of performance because
+ * of more frequent GC.
+ */
+
+#include <ruby.h>
+#include "lexbor/core/base.h"
+
+void *
+lexbor_malloc(size_t size)
+{
+    return ruby_xmalloc(size);
+}
+
+void *
+lexbor_realloc(void *dst, size_t size)
+{
+    return ruby_xrealloc(dst, size);
+}
+
+void *
+lexbor_calloc(size_t num, size_t size)
+{
+    return ruby_xcalloc(num, size);
+}
+
+void *
+lexbor_free(void *dst)
+{
+    ruby_xfree(dst);
+    return NULL;
+}
