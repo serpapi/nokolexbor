232f1bac54b9c7887a9ece8d80e4857ea7a55357
zyc9012 <zyc9012@163.com>
2022-12-07 09:28:22 +0800

    Require C extension at the beginning

--- lib/nokolexbor.rb
+++ lib/nokolexbor.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
+require 'nokolexbor/nokolexbor'
 require 'nokolexbor/version'
 require 'nokolexbor/node'
 require 'nokolexbor/node_set'
 require 'nokolexbor/attribute'
 require 'nokolexbor/xpath_context'
-require 'nokolexbor/nokolexbor'
