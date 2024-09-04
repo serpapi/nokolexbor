055f91e0f42e87779feca0d53f5e86b5538e9a59
zyc9012 <zyc9012@gmail.com>
2024-09-04 09:44:49 +0800

    Add multi thread test

---
+++ spec/multi_thread_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Multi threading" do
+  it 'should be thread-safe' do
+    10000.times.map do
+      Thread.new do
+        random_str = (0...32).map { (65 + rand(58)).chr }.join
+        doc = Nokolexbor::HTML('<div><a class="a">' + random_str + '</a></div>')
+        _(doc.css("div ::text").text).must_equal random_str
+      end
+    end.each(&:join)
+  end
+end
