5ef34e944cfd58a46000483b35ff8ba21bc71244
Thilo Rusche <thilorusche@gmail.com>
2026-02-23 12:07:49 +0000

    Added test to try and reproduce race condition

--- spec/multi_thread_spec.rb
+++ spec/multi_thread_spec.rb
@@ -10,4 +10,66 @@ describe "Multi threading" do
       end
     end.each(&:join)
   end
+
+  it 'returns correct results under concurrent access with diverse selectors' do
+    num_threads = 100
+    iterations = 50
+    errors = Queue.new
+    barrier = Queue.new
+
+    workers = num_threads.times.map do |i|
+      Thread.new do
+        barrier.pop
+        iterations.times do |j|
+          html = "<div id='t#{i}'><span class='c#{i}'>text-#{i}-#{j}</span><a href='#'>link-#{i}</a></div>"
+          doc = Nokolexbor::HTML(html)
+
+          result = doc.css(".c#{i}")
+          unless result.length == 1 && result.text == "text-#{i}-#{j}"
+            errors << "Thread #{i} iter #{j}: css got '#{result.text}' expected 'text-#{i}-#{j}'"
+          end
+
+          result = doc.at_css("#t#{i}")
+          unless result && result.name == 'div'
+            errors << "Thread #{i} iter #{j}: at_css failed"
+          end
+
+          result = doc.css(".c#{i}, a")
+          unless result.length == 2
+            errors << "Thread #{i} iter #{j}: css comma got #{result.length} nodes, expected 2"
+          end
+        end
+      end
+    end
+
+    num_threads.times { barrier << true }
+    workers.each(&:join)
+
+    collected = []
+    collected << errors.pop until errors.empty?
+    assert_equal 0, collected.size, collected.first(5).join("\n")
+  end
+
+  it 'does not retain error state from previous parse failures' do
+    doc = Nokolexbor::HTML('<div><span class="valid">text</span></div>')
+    invalid_selectors = [
+      'div:invalid-pseudo-class-that-does-not-exist',
+      '::text1',
+      ':not()',
+      '[attr=',
+      'div >>',
+    ]
+
+    # Aggressively cycle error-then-valid 1000 times with diverse invalid selectors.
+    # The static css_parser may be left in a dirty state (stage = LXB_CSS_PARSER_RUN)
+    # after a parse failure, causing subsequent valid queries to fail.
+    1000.times do |i|
+      sel = invalid_selectors[i % invalid_selectors.length]
+
+      _{ doc.css(sel) }.must_raise Nokolexbor::Lexbor::UnexpectedDataError
+      result = doc.css('span.valid')
+      _(result.length).must_equal 1, "Iteration #{i}: expected 1 node after error with '#{sel}', got #{result.length}"
+      _(result.text).must_equal 'text', "Iteration #{i}: expected 'text' after error with '#{sel}', got '#{result.text}'"
+    end
+  end
 end
