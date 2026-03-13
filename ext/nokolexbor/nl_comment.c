2dedd550eb2382b66d8f432374c3d10562ff563e
zyc9012 <zyc9012@gmail.com>
2026-03-13 14:45:37 +0800

    Support creating Node with Node (#19)

--- ext/nokolexbor/nl_comment.c
+++ ext/nokolexbor/nl_comment.c
@@ -1,6 +1,7 @@
 #include "nokolexbor.h"
 
 VALUE cNokolexborComment;
+extern VALUE cNokolexborNode;
 extern VALUE cNokolexborCharacterData;
 extern VALUE mNokolexbor;
 
@@ -20,11 +21,15 @@ nl_comment_new(int argc, VALUE *argv, VALUE klass)
 
   rb_scan_args(argc, argv, "2*", &rb_content, &rb_document, &rest);
 
-  if (!rb_obj_is_kind_of(rb_document, cNokolexborDocument)) {
-    rb_raise(rb_eArgError, "Document must be a Nokolexbor::Document");
+  if (rb_obj_is_kind_of(rb_document, cNokolexborDocument)) {
+    document = nl_rb_document_unwrap(rb_document);
+  } else if (rb_obj_is_kind_of(rb_document, cNokolexborNode)) {
+    lxb_dom_node_t *node = nl_rb_node_unwrap(rb_document);
+    document = node->owner_document;
+    rb_document = nl_rb_document_get(rb_document);
+  } else {
+    rb_raise(rb_eArgError, "Expected a Document or Node, got %s", rb_class2name(CLASS_OF(rb_document)));
   }
-
-  document = nl_rb_document_unwrap(rb_document);
 
   const char *c_content = StringValuePtr(rb_content);
   size_t content_len = RSTRING_LEN(rb_content);
